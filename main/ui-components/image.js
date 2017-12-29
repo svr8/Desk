96ac66c8dc2ca985c515e4038d1e4dfc7a7d5e36
svr8 <shikhar.vaish90@gmail.com>
2017-12-29 13:06:12 +0530

    Removed extra files

--- main/ui-components/image.js
+++
@@ -1,8 +0,0 @@
-function Image(className, def, sel) {
-    this.className = className;
-    this.def = def;
-    this.sel = sel;
-    this.renderHTML = function() {
-        return '<div class="'+this.className+'"></div>';
-    }
-}
